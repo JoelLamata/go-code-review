046a22fc43e9217474e7ba5be85461dc5c68ce4d
Joel Lamata <jlamata88@gmail.com>
2024-05-09 14:37:48 +0200

    deleted unnecessary interface, initialization of entries, rename and check entries overwriting

--- review/internal/repository/memdb/memdb.go
+++ review/internal/repository/memdb/memdb.go
@@ -7,28 +7,29 @@ import (
 
 type Config struct{}
 
-type repository interface {
-	FindByCode(string) (*entity.Coupon, error)
-	Save(entity.Coupon) error
-}
-
 type Repository struct {
 	entries map[string]entity.Coupon
 }
 
 func New() *Repository {
-	return &Repository{}
+	return &Repository{
+		entries: make(map[string]entity.Coupon),
+	}
 }
 
 func (r *Repository) FindByCode(code string) (*entity.Coupon, error) {
-	coupon, ok := r.entries[code]
-	if !ok {
-		return nil, fmt.Errorf("Coupon not found")
+	coupon, found := r.entries[code]
+	if !found {
+		return nil, fmt.Errorf("coupon not found")
 	}
 	return &coupon, nil
 }
 
 func (r *Repository) Save(coupon entity.Coupon) error {
+	_, found := r.entries[coupon.Code]
+	if found {
+		return fmt.Errorf("coupon with code %s already exists", coupon.Code)
+	}
 	r.entries[coupon.Code] = coupon
 	return nil
 }
